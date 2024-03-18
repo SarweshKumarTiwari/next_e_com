9068698bec67250e5d04152a24527527d273097b
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2024-03-18 19:24:14 +0530

    alert initialized

---
+++ src/components/ui/alert.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+type alertType={
+    trigger:JSX.Element
+    description:string
+    title:string
+    onContinue:()=>void
+}
+export default function Alert({trigger,description,title,onContinue}:alertType) {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {trigger}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>{title}</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        {description}
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+
+    )
+}
