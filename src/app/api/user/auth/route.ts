b1368ee72b98c1bf4a0b14ed2030a5801f8a8620
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2024-03-18 19:29:53 +0530

    added auth

---
+++ src/app/api/user/auth/route.ts
@@ -0,0 +1,31 @@
+import asyncHandler from "@/lib/asyncHandler";
+import ApiError from "@/lib/error";
+import { NextRequest, NextResponse } from "next/server";
+import userModel from "@/models/user"
+
+export async function  POST(req:NextRequest){
+    return await asyncHandler(async (req)=>{
+        const body=await req.json();
+        if (!(body.email&&body.password)) {
+            throw new ApiError(400,"no valid email or password");
+        }
+        const user=await userModel.findOne({email:body.email});
+        if (!user&&!user.compare(body.password)) {
+            throw new ApiError(400,"no user found or incorrect password");
+        }
+        const response=NextResponse.json({
+            success:"logged"
+        },{status:200})
+        response.cookies.set("rt",user.accessToken());
+        return response;
+    })(req);
+}
+
+export async function DELETE(req:NextRequest){
+    return await asyncHandler(async (req)=>{
+        const response=NextResponse.json({success:"logged out"},{status:200});
+        response.cookies.set("at","");
+        return response;
+
+    })(req);
+}
