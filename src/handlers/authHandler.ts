813a82db86c6612a61185553f330b27d6073d59e
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2024-03-18 19:30:37 +0530

    auth handler to handle user authentication

---
+++ src/handlers/authHandler.ts
@@ -0,0 +1,12 @@
+import ApiError from "@/lib/error";
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken"
+
+export default function authHandler(req:NextRequest){
+    const cookie=req.cookies.get("at");
+    if (!cookie?.value) {
+        throw new ApiError(400,"unauthorised");
+    }
+    const data=jwt.verify(cookie.value,process.env.ACCESS_TOKEN as string);
+    return data;
+}
