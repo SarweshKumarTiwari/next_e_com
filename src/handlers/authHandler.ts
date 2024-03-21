0f583a8d843b0f7216e290e173de2e15452714b9
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2024-03-21 22:53:03 +0530

    implemented a custom payload

--- src/handlers/authHandler.ts
+++ src/handlers/authHandler.ts
@@ -1,12 +1,16 @@
 import ApiError from "@/lib/error";
 import { NextRequest } from "next/server";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 
+interface custom extends JwtPayload{
+    id:string,
+    name:string
+}
 export default function authHandler(req:NextRequest){
     const cookie=req.cookies.get("at");
     if (!cookie?.value) {
         throw new ApiError(400,"unauthorised");
     }
-    const data=jwt.verify(cookie.value,process.env.ACCESS_TOKEN as string);
-    return JSON.parse(data as string);
+    const data=jwt.verify(cookie.value,process.env.ACCESS_TOKEN as string) as custom;
+    return data;
 }
