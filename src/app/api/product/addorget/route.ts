5b2c45ad324f7baecdb48534774cd0d339245ae7
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2024-03-21 22:56:23 +0530

    added product route

--- src/app/api/product/addorget/route.ts
+++ src/app/api/product/addorget/route.ts
@@ -3,16 +3,6 @@ import ApiError from "@/lib/error";
 import productModel from "@/models/product";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST (req:NextRequest){
-    return await asyncHandler(async (req)=>{
-        const body=await req.json();
-        if (!(body.price&&body.category&&body.seller&&body.title)) {
-            throw new ApiError(400,"please provide price,category,seller and title")
-        }
-        await new productModel(body).save();
-        return NextResponse.json({success:'added product'},{status:200});
-    })(req);
-}
 
 export async function GET (req:NextRequest){
     return await asyncHandler(async (req)=>{
